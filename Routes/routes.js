a4376a3b61c0c9768cb9e216d06f31aaa6c51782
Yoliber Pichardo <yoliberfelixpp01@gmail.com>
2022-08-01 16:38:43 -0400

    added get user

--- Routes/routes.js
+++ Routes/routes.js
@@ -5,12 +5,12 @@ const Router =  express.Router
 
 const router = Router()
 
-router.get('/number_friends', verifyJWT, getFriendsNumber)
-router.post('/myuser', verifyJWT, myUser)
-router.post('/user',verifyJWT,viewUser)
 router.post('/register',register)
 router.post('/login', loginUser)
-router.put('/edit/:id', editUser)
+router.get('/number_friends', verifyJWT, getFriendsNumber)
+router.get('/myuser', verifyJWT, myUser)
+router.get('/user',verifyJWT,viewUser)
+router.put('/edit/:id', verifyJWT,editUser)
 router.post('/resquet_send',verifyJWT, resquetSend)
 router.post('/get_users',verifyJWT, getUsersByID)
 
